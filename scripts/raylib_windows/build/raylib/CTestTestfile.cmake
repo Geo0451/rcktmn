63a9e25887ec23433f4b1cebc8871cf4af689f2a
Geo0451 <geokj0451@gmail.com>
2026-06-05 22:32:32 +0530

    -Removed raylib source files uses to statically link windows and web executables

--- scripts/raylib_windows/build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/g_kj/Documents/code/rcktmn/rcktmn/scripts/raylib_windows/raylib/src
-# Build directory: /home/g_kj/Documents/code/rcktmn/rcktmn/scripts/raylib_windows/build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
